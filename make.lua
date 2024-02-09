30ee72397908df34e8b0498c8fc3540a385e090e
Matthew Brooks <mb964033@gmail.com>
2024-02-09 00:51:28 +0000

    update libs

--- make.lua
+++ make.lua
@@ -1,7 +1,7 @@
 local env = {
 
   name = "santoku-porter",
-  version = "0.0.4-1",
+  version = "0.0.5-1",
   variable_prefix = "TK_PORTER",
   license = "MIT",
   public = true,
@@ -12,8 +12,7 @@ local env = {
 
   test = {
     dependencies = {
-      "santoku-test >= 0.0.7-1",
-      "luassert >= 1.9.0-1",
+      "santoku >= 0.0.191-1",
       "luacov >= 0.15.0-1",
     }
   },
--- test/spec/santoku/porter.lua
+++ test/spec/santoku/porter.lua
@@ -1,9 +1,8 @@
-local assert = require("luassert")
 local test = require("santoku.test")
 local porter = require("santoku.porter")
 
 test("porter", function ()
-  assert.equals("like", porter("likes"))
-  assert.equals("literari", porter("literary"))
-  assert.equals("literari", porter("literaries"))
+  assert("like" == porter("likes"))
+  assert("literari" == porter("literary"))
+  assert("literari" == porter("literaries"))
 end)
